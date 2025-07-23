08950cdd9f9dd9bd2060b6f4ba1487104ff2c6e7
prasanthvercel <prasanthvercel@gmail.com>
2025-07-23 11:05:05 +0000

    Changes before Firebase Studio auto-run

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import "styles/tailwind.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-rebeccapurple">{children}</body> {/* Added bg-rebeccapurple class */}
+      <head> {/* Added head tag */}
+        <link rel="icon" href="/logo.jpg" /> {/* Added favicon link */}
+      </head>
+      <body className="bg-rebeccapurple">{children}</body>
     </html>
   );
 }
