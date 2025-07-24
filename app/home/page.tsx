f5c052bdae7b2bc8c82a2a57eb956056ffcb8463
prasanthvercel <prasanthvercel@gmail.com>
2025-07-24 06:00:53 +0000

    home screen errors

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from 'components/Button/Button';
 
 const HomePage = () => {
